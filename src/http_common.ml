c396c1397c6c1d23686e569ede8fcac6317e2724
dueruen <32197042+dueruen@users.noreply.github.com>
2021-04-29 19:28:58 +0200

    Test for get file list

--- src/http_common.ml
+++ src/http_common.ml
@@ -1,4 +1,5 @@
 open Ezcurl
+open QCheck
 
 type status_code = HttpOk | NotFound | BadRequest | Unauthorized | NotAcceptable | Conflict| InternalServerError | Unknown of int
 
@@ -14,6 +15,8 @@ let status_code_of_int n = match n with
 
 type response = {
   status_code: status_code;
+  content_type: string option;
+}
   (* x_file_version: string option;
   x_file_path: string option;
   x_file_path_encoded: string option;
@@ -21,10 +24,8 @@ type response = {
   x_conflict: string option;
   (* x_access_denied: string option; *)
   x_bytes: string option; *)
-  content_type: string option;
   (* conection: string option;
   transfer_encoding: string option; *)
-}
 
 let rec map_header (headers: (string * string) list) (lookAfter: string): string option =
   match headers with
@@ -37,23 +38,41 @@ let rec map_header (headers: (string * string) list) (lookAfter: string): string
 
 let map_response (http_res: Ezcurl_core.response): response = {
   status_code = status_code_of_int http_res.code;
-  content_type = map_header http_res.headers "Content-Type"
+  content_type = map_header http_res.headers "Content-Type";
 }
 
 
 let url = "http://localhost:8085/file/list?userId=1";;
 
-let print_ltuples l = List.iter (fun (a,b) -> Printf.printf "%s, %s \n" a b) l
+let print_ltuples l = List.iter (fun (a,b) -> Printf.printf "%s, %s \n" a b) l;;
 
-let _ = match Ezcurl.get ~url: url () with
- | Ok resp -> 
+(* let _ = match Ezcurl.get ~url: url () with
+ | Ok resp ->
     let resss = map_header resp.headers "Content-Type" in
-    match resss with
-    | Some(a) -> Printf.printf "HEADER::::: %s \n" a
-    | None -> Printf.printf "NO HEADER::::: %s \n" "None"
-    
- | _ -> ()
+    let s = (match resss with
+      | Some(a) -> Printf.printf "HEADER::::: %s \n" a
+      | None -> Printf.printf "NO HEADER::::: %s \n" "None") in
+    1
+ | Error (_) -> 1
+;; *)
+
+let checkGetListOfFiles (userId: int): int = 
+  match Ezcurl.get ~url: url () with
+  | Ok resp -> (
+      match map_response resp with
+      | { status_code = HttpOk; _} -> 1
+      | _ -> 0
+    )
+  | Error (_) -> 0
 ;;
+
+let get_file_list = 
+  Test.make ~name:"get_file_list" ~count:1000 ~max_gen:500  
+  int
+  (fun i -> checkGetListOfFiles i == 1)
+;;
+QCheck_runner.run_tests ~verbose:true [get_file_list];;
 
 (* let res = Ezcurl.get ~url: url ();;
 let moe = map_response  *)
+
