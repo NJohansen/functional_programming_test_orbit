b2a1cb80d9a44d5dd9498529fbbda864414a1e5a
Kasper <kaspermn33@gmail.com>
2021-05-18 16:11:05 +0200

    added to machine.ml and fixed error in getfilemeta

--- src/machine.ml
+++ src/machine.ml
@@ -4,6 +4,7 @@ open Filelist
 open Getfile
 open Deletefile
 open Deletedir
+open Getfilemeta
 
 module CConf =
 struct
@@ -13,7 +14,8 @@ struct
     | Get_File_List of int 
     | Get_File of int * int 
     | Delete_File of int * int * int
-    | Delete_Dir of int * int * int [@@deriving show { with_path = false }]
+    | Delete_Dir of int * int * int 
+    | Get_File_Meta of int * int [@@deriving show { with_path = false }]
     (* | Create_File of (int) (int) (int) (int) *)
 
   let gen_cmd (st: state) =
@@ -51,7 +53,8 @@ struct
       [ Gen.map (fun userId -> Get_File_List userId) user_id_gen;
         Gen.map2 (fun userId fileId -> Get_File (userId, fileId)) user_id_gen file_id_gen;
         Gen.map3 (fun userId fileId version -> Delete_File (userId, fileId, version)) user_id_gen file_id_gen version_gen;
-        Gen.map3 (fun userId dirId version -> Delete_Dir (userId, dirId, version)) user_id_gen dir_id_gen version_gen]
+        Gen.map3 (fun userId dirId version -> Delete_Dir (userId, dirId, version)) user_id_gen dir_id_gen version_gen;
+        Gen.map2 (fun userId fileId -> Get_File_Meta (userId, fileId)) user_id_gen file_id_gen]
 
   let arb_cmd (st: state) = QCheck.make ~print:show_cmd (gen_cmd st)
 
@@ -64,6 +67,7 @@ struct
     | Get_File _ -> st
     | Delete_File (userId, fileId, version) -> Deletefile.deleteFileUpdateState userId fileId version st
     | Delete_Dir (userId, dirId, version) -> Deletedir.deleteDirectoryUpdateState userId dirId version st
+    | Get_File_Meta _ -> st
 
   let init_sut () = (Printf.printf "----------------\n"; Orbit.orbit_state)
   let cleanup _   = ()
@@ -76,6 +80,8 @@ struct
       (Printf.printf "Delete file, user: %d - file: %d - version: %d \n" userId fileId version; Deletefile.checkDeleteFile userId fileId version !su)      
     | Delete_Dir (userId, dirId, version) -> 
       (Printf.printf "Delete dir, user: %d - dir: %d - version: %d \n" userId dirId version; Deletedir.checkDeleteDirectory userId dirId version !su)
+    | Get_File_Meta (userId, fileId) ->
+      (Printf.printf "Get file metadata, user: %d - file: %d \n" userId fileId; Getfilemeta.checkFileMeta userId fileId !su)
 
   let precond _ _ = true
 end
