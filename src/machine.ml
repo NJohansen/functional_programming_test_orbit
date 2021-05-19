8532fc87e27605a2bd0e329a7ba3a828d8cdf3a5
dueruen <32197042+dueruen@users.noreply.github.com>
2021-05-19 11:52:06 +0200

    Get version

--- src/machine.ml
+++ src/machine.ml
@@ -4,6 +4,7 @@ open Filelist
 open Getfile
 open Deletefile
 open Deletedir
+open Getversion
 
 module CConf =
 struct
@@ -11,7 +12,8 @@ struct
   type sut   = Orbit.system ref
   type cmd   =
     | Get_File_List of int 
-    | Get_File of int * int 
+    | Get_File of int * int
+    | Get_Version of int * string 
     | Delete_File of int * int * int
     | Delete_Dir of int * int * int [@@deriving show { with_path = false }]
     (* | Create_File of (int) (int) (int) (int) *)
@@ -47,9 +49,13 @@ struct
         Gen.small_signed_int;
       ] in
 
+    let version_string_gen =
+      Gen.map3 (fun f1 f2 f3 -> Printf.sprintf "%d.%d.%d.%d.%d.%d" f1 f2 f3 f3 f2 f1) Gen.small_signed_int Gen.small_signed_int Gen.small_signed_int in
+
     Gen.oneof
       [ Gen.map (fun userId -> Get_File_List userId) user_id_gen;
         Gen.map2 (fun userId fileId -> Get_File (userId, fileId)) user_id_gen file_id_gen;
+        Gen.map2 (fun userId versionString -> Get_Version (userId, versionString)) user_id_gen version_string_gen;
         Gen.map3 (fun userId fileId version -> Delete_File (userId, fileId, version)) user_id_gen file_id_gen version_gen;
         Gen.map3 (fun userId dirId version -> Delete_Dir (userId, dirId, version)) user_id_gen dir_id_gen version_gen]
 
@@ -61,6 +67,7 @@ struct
   let next_state c st = match c with
     | Get_File_List _ -> Orbit.next_state_done !st
     | Get_File _ -> Orbit.next_state_done !st
+    | Get_Version _ -> Orbit.next_state_done !st
     | Delete_File (userId, fileId, version) -> Deletefile.deleteFileUpdateState userId fileId version st
     | Delete_Dir (userId, dirId, version) -> Deletedir.deleteDirectoryUpdateState userId dirId version st
 
@@ -71,6 +78,8 @@ struct
       (Printf.printf "Get file list, user: %d \n" userId; Filelist.checkGetListOfFiles userId !st)
     | Get_File (userId, fileId) -> 
       (Printf.printf "Get file, user: %d - file: %d \n" userId fileId; Getfile.checkGetFile userId fileId !st)
+    | Get_Version (userId, versionString) -> 
+      (Printf.printf "Get version, user: %d - versionString: %s \n" userId versionString; Getversion.checkVersion userId versionString !st)    
     | Delete_File (userId, fileId, version) -> 
       (Printf.printf "Delete file, user: %d - file: %d - version: %d \n" userId fileId version; Deletefile.checkDeleteFile userId fileId version !st)      
     | Delete_Dir (userId, dirId, version) -> 
